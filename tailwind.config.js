f2b1e1a1df92dfd0342fcc5f71a8d0e3a6e8c506
paulMatie208 <paul.martie.tech@gmail.com>
2024-05-27 00:54:09 +0200

    add number game section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,22 @@ export default {
       colors: {
         black: "#000000",
         white: "#ffffff",
-        blue: "#4781FF"
+        white_1: "#f7f8ff",
+        blue: "#4781FF",
+        black_1: "#b2b2b2",
+        black_2: "#eeeeee",
+        red: "#fd565c",
+        green: "#40ad72",
+        purple: "#ec4cdf",
       },
       container: {},
       extend: {
         fontFamily: {},
         backgroundImage: {},
         backgroundColor: {},
-        boxShadow: {},
+        boxShadow: {
+          white_shadow: "0px 10px .2px 10px #c5c5da40",
+        },
         screens: {},
       },
     },
